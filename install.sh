f48ed4e9e878aa605fd4f8308f431911a712ba13
Mordi Sacks <mordisacks@gmail.com>
2016-03-10 01:30:46 +0200

    added sypanel user

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ apt-get update -qq -y --force-yes
 
 apt-get install lsb-release -qq -y --force-yes
 
-# check for debian
+# Check for debian
 OS_NAME=$(lsb_release -si)
 
 if [ ${OS_NAME,,} != 'debian' ]; then
@@ -29,18 +29,18 @@ apt-get upgrade -qq -y --force-yes
 
 echo "Installing necessary packages"
 
-apt-get install wget sed git unzip curl --force-yes -qq -y
+apt-get install wget sudo sed git unzip curl --force-yes -qq -y
 
 CODENAME=$(grep "VERSION=" /etc/os-release |awk -F= {' print $2'}|sed s/\"//g |sed s/[0-9]//g | sed s/\)$//g |sed s/\(//g)
 
-# install nginx
+# Install nginx
 echo "deb http://nginx.org/packages/debian/ $CODENAME nginx" > /etc/apt/sources.list.d/nginx.list
 echo "deb-src http://nginx.org/packages/debian/ $CODENAME nginx" >> /etc/apt/sources.list.d/nginx.list
 wget -qO - http://nginx.org/keys/nginx_signing.key | apt-key add -
 apt-get update -qq -y --force-yes 
 apt-get install -qq -y nginx nginx-extras
 
-#install MySQL
+# Install MySQL
 _PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 echo mysql-server mysql-server/root_password password $_PASSWORD | sudo debconf-set-selections
 echo mysql-server mysql-server/root_password_again password $_PASSWORD | sudo debconf-set-selections
@@ -49,15 +49,15 @@ echo $_PASSWORD > ~/.my.cnf
 apt-get install --force-yes -y -qq mariadb-server-10.0
 apt-get install --force-yes -y -qq bind9
 
-#clearing
+# Clearing
 unset _PASSWORD
 unset DEBIAN_FRONTEND
 
-#finally install PHP
+# Finally install PHP
 
 apt-get install --force-yes -y -qq php5-fpm php5-cli php5-gd php5-curl php5-json php5-mcrypt
 
-# fix cgi.fix_pathinfo
+# Fix cgi.fix_pathinfo
 sed -i -- 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
 
 apt-get remove --purge --force-yes -y -qq exim4
@@ -65,15 +65,7 @@ apt-get remove --purge --force-yes -y -qq exim4
 apt-get install --force-yes -y -qq postfix 
 apt-get install --force-yes -y -qq dovecot-core dovecot-imapd dovecot-mysql dovecot-pop3d #dovecot-antispam
 
-#install SYpanel
-mkdir /usr/share/sypanel && cd /usr/share/sypanel
-
-wget https://github.com/SYpanel/SYpanel/archive/master.zip
-unzip master.zip
-
-cd ~
-
-# setup skel
+# Setup skel
 mkdir /etc/skel/public_html
 mkdir /etc/skel/tmp
 mkdir /etc/skel/ssl
@@ -81,3 +73,26 @@ mkdir /etc/skel/log
 cd /etc/skel
 ln -s public_html/ www
 cd ~
+
+# Create SYpanel user
+_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+useradd -d /home/sypanel -m -s /bin/bash -p $_PASSWORD sypanel
+unset _PASSWORD
+
+# Allow SYpanel user to sudo without password
+printf "\n\n#SYpanel user\nsypanel ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Download SYpanel GUI
+##TOFIX-THINK
+cd /home/sypanel/public_html
+
+wget https://github.com/SYpanel/SYpanel/archive/master.zip
+unzip master.zip
+
+cd ~
+
+# Create fpm pool for sypanel
+##TODO
+
+# Create nginx conf for sypanel
+##TODO
