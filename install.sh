5af9ab2a9822e9359d145717a769c4da294be9f0
Mordi Sacks <mordisacks@gmail.com>
2016-03-11 00:13:20 +0200

    added key generate

--- install.sh
+++ install.sh
@@ -98,6 +98,7 @@ git clone https://github.com/SYpanel/SYpanel.git .
 
 wget --no-check-certificate -q "https://raw.githubusercontent.com/SYpanel/installer/$SY_VERSION/.env" -O /home/sypanel/public_html/.env
 sed -i -- 's/DB_PASSWORD=secret/DB_PASSWORD=$_PASSWORD_DB/g' .env
+php artisan key:generate
 cd ~
 
 chown -R sypanel:sypanel /home/sypanel
