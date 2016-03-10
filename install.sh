7a67e43a1c58a24e4050bc40ab8cb3678367c388
Mordi Sacks <mordisacks@gmail.com>
2016-03-11 00:07:50 +0200

    added stuff

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # SYpanel installer
-SY_VERSION="0.0.1" 
+SY_VERSION="0.0.2" 
 
 echo "SYpanel $SY_VERSION";
 
@@ -12,9 +12,9 @@ command -v apt-get >/dev/null 2>&1 || { echo "I require apt-get but it's not ins
 
 echo "Updating reposetories"
 
-apt-get update -qq -y --force-yes
+apt-get update -y --force-yes
 
-apt-get install lsb-release -qq -y --force-yes
+apt-get install lsb-release -y --force-yes
 
 # Check for debian
 OS_NAME=$(lsb_release -si)
@@ -25,11 +25,11 @@ if [ ${OS_NAME,,} != 'debian' ]; then
 fi
 echo "Upgrading packages"
 
-apt-get upgrade -qq -y --force-yes
+apt-get upgrade -y --force-yes
 
 echo "Installing necessary packages"
 
-apt-get install wget sudo sed git unzip curl --force-yes -qq -y
+apt-get install wget sudo sed git unzip curl --force-yes -y
 
 CODENAME=$(grep "VERSION=" /etc/os-release |awk -F= {' print $2'}|sed s/\"//g |sed s/[0-9]//g | sed s/\)$//g |sed s/\(//g)
 
@@ -37,17 +37,22 @@ CODENAME=$(grep "VERSION=" /etc/os-release |awk -F= {' print $2'}|sed s/\"//g |s
 echo "deb http://nginx.org/packages/debian/ $CODENAME nginx" > /etc/apt/sources.list.d/nginx.list
 echo "deb-src http://nginx.org/packages/debian/ $CODENAME nginx" >> /etc/apt/sources.list.d/nginx.list
 wget -qO - http://nginx.org/keys/nginx_signing.key | apt-key add -
-apt-get update -qq -y --force-yes 
-apt-get install -qq -y nginx nginx-extras
+apt-get update -y --force-yes 
+apt-get install -y nginx nginx-extras
 
 # Install MySQL
 _PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo mysql-server mysql-server/root_password password $_PASSWORD | sudo debconf-set-selections
-echo mysql-server mysql-server/root_password_again password $_PASSWORD | sudo debconf-set-selections
-echo "MySQL root password is $_PASSWORD"
-echo $_PASSWORD > ~/.my.cnf
-apt-get install --force-yes -y -qq mariadb-server-10.0
-apt-get install --force-yes -y -qq bind9
+sudo debconf-set-selections <<< "mariadb-server-10.0 mysql-server/root_password password $_PASSWORD"
+sudo debconf-set-selections <<< "mariadb-server-10.0 mysql-server/root_password_again password $_PASSWORD"
+
+echo "MySQL root password is $_PASSWORD the password is in ~/.mysql_pass"
+echo $_PASSWORD > ~/.mysql_pass
+apt-get install --force-yes -y mariadb-server-10.0
+apt-get install --force-yes -y bind9
+
+# Create sypanel mysql user and db
+_PASSWORD_DB=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+mysql -uroot -p"$_PASSWORD" -e "CREATE DATABASE sypanel;GRANT ALL PRIVILEGES ON sypanel.* TO sypanel@localhost IDENTIFIED BY 'n$_PASSWORD_DB'";
 
 # Clearing
 unset _PASSWORD
@@ -55,15 +60,18 @@ unset DEBIAN_FRONTEND
 
 # Finally install PHP
 
-apt-get install --force-yes -y -qq php5-fpm php5-cli php5-gd php5-curl php5-json php5-mcrypt
+apt-get install --force-yes -y php5-fpm php5-cli php5-gd php5-curl php5-json php5-mcrypt
 
 # Fix cgi.fix_pathinfo
 sed -i -- 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
 
-apt-get remove --purge --force-yes -y -qq exim4
+apt-get remove --purge --force-yes -y exim4
 
-apt-get install --force-yes -y -qq postfix 
-apt-get install --force-yes -y -qq dovecot-core dovecot-imapd dovecot-mysql dovecot-pop3d #dovecot-antispam
+debconf-set-selections <<< "postfix postfix/mailname string sypanel"
+debconf-set-selections <<< "postfix postfix/main_mailer_type string 'No configuration'"
+
+apt-get install --force-yes -y postfix 
+apt-get install --force-yes -y dovecot-core dovecot-imapd dovecot-mysql dovecot-pop3d #dovecot-antispam
 
 # Setup skel
 mkdir /etc/skel/public_html
@@ -87,7 +95,14 @@ printf "\n\n#SYpanel user\nsypanel ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 cd /home/sypanel/public_html
 rm -rf *
 git clone https://github.com/SYpanel/SYpanel.git .
+
+wget --no-check-certificate -q "https://raw.githubusercontent.com/SYpanel/installer/$SY_VERSION/.env" -O /home/sypanel/public_html/.env
+sed -i -- 's/DB_PASSWORD=secret/DB_PASSWORD=$_PASSWORD_DB/g' .env
 cd ~
+
+chown -R sypanel:sypanel /home/sypanel
+find /home/sypanel/www -type d -exec chmod 0755 {} \;
+find /home/sypanel/www -type f -exec chmod 0644 {} \;
 
 # Remove default pool
 mv /etc/php5/fpm/pool.d/www.conf /etc/php5/fpm/pool.d/www.bak
@@ -101,6 +116,6 @@ service php5-fpm reload
 mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bak
 
 # Fetch nginx conf for sypanel
-wget --no-check-certificate -q "https://raw.githubusercontent.com/SYpanel/installer/$SY_VERSION/nginx.conf" -O /etc/nginx/sypanel.conf
+wget --no-check-certificate -q "https://raw.githubusercontent.com/SYpanel/installer/$SY_VERSION/nginx.conf" -O /etc/nginx/conf.d/sypanel.conf
 
 service nginx reload
